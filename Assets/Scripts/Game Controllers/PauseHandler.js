a77809d4c6571faa5c9a3f185993e724f939803d
keijiro <keijiro@gmail.com>
2012-05-09 22:37:47 +0900

    Source code refinement.

--- Assets/Scripts/Game Controllers/PauseHandler.js
+++ Assets/Scripts/Game Controllers/PauseHandler.js
@@ -1,33 +1,30 @@
 #pragma strict
 
-// ポーズを発動するスクリプト
-
 static var paused : boolean;
 
-// ゲーム開始/終了メッセージの受信
 function OnGameStart() {
     enabled = true;
 }
+
 function OnGameEnd() {
     enabled = false;
 }
 
-// ポーズ解除メッセージの受信
 function ResumeGame() {
     paused = false;
 }
 
-function OnApplicationPause(pause : boolean) {
-    // アプリケーションの動作がバックグラウンドに入った場合にポーズを発動する。
-    if (enabled && !paused && pause) {
+function OnApplicationPause(toPause : boolean) {
+    // Pause the game when the application goes to the background.
+    if (enabled && !paused && toPause) {
         SendMessage("PauseGame");
         paused = true;
     }
 }
 
 function Update () {
-    // 画面中央付近をタップしたときにポーズを発動する。
-    if (Input.GetButtonDown("Fire1")) {
+    // Pause the game if the screen is tapped.
+    if (Input.GetMouseButtonDown(0)) {
         var sw = Screen.width;
         var sh = Screen.height;
         var rect = Rect(0.05 * sw, 0.05 * sh, 0.9 * sw, 0.9 * sh);
