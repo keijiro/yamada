cc255b2e9927457a43d6e844d693e1b607111d60
keijiro <keijiro@gmail.com>
2012-05-09 23:08:33 +0900

    Stop the game going to sleep.

--- Assets/Scripts/System/Startup.js
+++ Assets/Scripts/System/Startup.js
@@ -7,6 +7,8 @@ function Start() {
     Social.localUser.Authenticate(function(result : boolean){});
 #endif
 
+    Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
     // Set the fintering mode of the GUI font to point-sampling.
     //   (I know that this should be set in build time,
     //   but somehow it can't. So I do this here...)
